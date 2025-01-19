d54fd211f761f93e08af7c4ac114bbf7f80da35b
Anjali-Bhanushali <anjalisinghbhanushali@gmail.com>
2025-01-19 08:33:02 +0000

    container config done

--- 01_Basics/03_conversionsOperations.js
+++ 01_Basics/03_conversionsOperations.js
@@ -30,4 +30,40 @@ let stringNumber = String(someNumber);
 //  ***************** Operations ******************
 let value = 3
 let negvalue = -value
-console.log(negvalue);
+//console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "hello";
+let str2 = " hitesh";
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2+ "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // this is not preferred code readability is important
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+//link to study
+//https://tc39.ex/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+ 
