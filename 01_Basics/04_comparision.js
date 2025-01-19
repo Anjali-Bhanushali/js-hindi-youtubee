e0f16ee00c1721f9e7059578f5e2636f3826cd8a
Anjali-Bhanushali <anjalisinghbhanushali@gmail.com>
2025-01-19 09:06:47 +0000

    container config done

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// comparision and equality check are two different things in javascript
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//=== strict check not only checks yhe value but also checks the datatype of the value
+
+console.log("2" === 2);
+console.log("2" == 2);
+
+
