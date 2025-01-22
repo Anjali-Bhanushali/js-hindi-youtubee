cc9e63cb5580c477e47ce0dec4e4bbe4e8a54204
Anjali-Bhanushali <anjalisinghbhanushali@gmail.com>
2025-01-22 07:43:01 +0000

    datatypes summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,72 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)    //we are using node.js, not browser
+
+let name = "Sheel"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to the power 53
+//bigint
+//string => " "
+//boolean => true or false
+//null => null value or standalone value
+//undefined => undefined variable
+// symobol => unique
+
+
+//object => {}
+//array => []
+//function => function(){}
+
+/************************ typeof ************************/
+
+console.log(typeof name);   //string
+console.log(typeof null);   //object
+/*
+    typeof "John"                 // Returns "string"
+    typeof 3.14                   // Returns "number"
+    typeof NaN                    // Returns "number"
+    typeof false                  // Returns "boolean"
+    typeof [1,2,3,4]              // Returns "object"    //In JavaScript, arrays are technically objects, so typeof always returns "object" for arrays.
+    typeof {name:'John', age:34}  // Returns "object"
+    typeof new Date()             // Returns "object"
+    typeof function () {}         // Returns "function"
+    typeof myCar                  // Returns "undefined" *
+    typeof null                   // Returns "object"
+*/
+
+/************************* TYPES OF DATATYPES ****************************/
+
+/*
+    1. Primitive
+        7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+    2. Reference (Non Primitive)
+        Array, Objects, Functions
+*/
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// In JavaScript, Symbol values are unique, even if they have the same description
+console.log(id==anotherId);     //false
+
+const bigNumber = 34567897815566558n    //bigInt datatype
+
+/************************ Array, Objects, Functions **************************/
+
+/* 
+    Array:
+    const cricketers = ["Virat", "Dhoni", "Rahul"];
+
+    Object:
+    let myObj = {
+        name: "Sheel",
+        age: 20,
+    }
+
+    Function:
+    const myFunction = function(){
+        console.log("Hello Sheel")
+    }
+*/
