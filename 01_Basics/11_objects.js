1b8c6938cae8217251a50e429acc1b2f174d173c
Anjali-Bhanushali <anjalisinghbhanushali@gmail.com>
2025-03-22 08:32:12 +0000

    container config done

---
+++ 01_Basics/11_objects.js
@@ -0,0 +1,19 @@
+//singleton
+//object.create
+
+//==============
+
+//object literals
+
+const user = {
+    name:"anjali",
+    age:18,
+    email: "hello@123.com",
+    location: "bihar",
+    isloggedin: false,
+    lastloggedin: ["monday","tuesday"]
+}
+
+console.log(user.email)
+console.log(user["email"])
+
